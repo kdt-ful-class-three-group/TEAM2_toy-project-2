8b6eda2d7ab3dad8262a898da035b01a049677bd
김윤지 <yunw0117@gmail.com>
2025-03-06 16:26:43 +0900

    scritp.js파일 생성

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const texterea = document.getElementById("content");
+
+let timeToSave;
+
+// content Input에 작성 안할시 타이머 설정
+texterea.addEventListener("input", function(){
+  const content = texterea.value;
+  console.log(content);
+  clearTimeout(timeToSave); // 이전 타이머 제거
+
+  timeToSave = setTimeout(() => {
+    console.log("저장해야합니다. 5초 지났습니다.");
+    localStorage.setItem("content", content);
+  }, 5000);
+});
+
+// 
+
+

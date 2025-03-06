7568083d431015583253333d452b4e8fd0098b95
김윤지 <yunw0117@gmail.com>
2025-03-06 17:48:54 +0900

    임시저장 문구 생성

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ const texterea = document.getElementById("content");
 const port = 3000
 
 let timeToSave;
-
+// p태그를 가져옴
+let pText = document.getElementById('saveText')
+// p태그에 display-none class 추가
+pText.classList.add("display-none")
 // content Input에 작성 안할시 타이머 설정
 texterea.addEventListener("input", function(){
   const content = texterea.value;
   console.log(content);
   clearTimeout(timeToSave); // 이전 타이머 제거
+  // 입력중일때 p태그에 display-none class 추가(입력중일 때 p태그 안 보임)
+  pText.classList.add("display-none")
+
+
 
   timeToSave = setTimeout(() => {
     console.log("저장해야합니다. 5초 지났습니다.");
     localStorage.setItem("content", content);
+
+    // 5초 후에 p태그 보임
+    pText.classList.remove("display-none")
   }, 5000);
 });
 

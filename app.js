0d34fc15160da122634b6a7cbf9d3ac2ba6bb391
최정민 <bigdragon00@naver.com>
2025-03-07 09:39:38 +0900

    sub-test 브랜치 생성, 이 브랜치에서 기능 테스트 후 test -> main 순으로 작업 푸쉬

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3002;
 const path = require('path');
+const fs = require('fs');
+
 
 
 app.use(express.static("public"))
+app.use(express.json());
+let jsonData = "/dataprac.json"
 
 
 app.get('/', function(req,res) {
@@ -12,8 +16,40 @@ app.get('/', function(req,res) {
 })
 
 
+app.get('/dataprac', function(req,res) {
+
+    res.sendFile(path.join(__dirname, "dataprac.json"))
+
+
+
+})
+
+
+
+
 app.post("/write", function(req,res) {
     console.log("write 요청 받음")
+    const data = req.body;
+
+
+    jsonData.push(data)
+    res.redirect("/")
+
+
+
+
+
+
+    // const body = req.body;
+    //
+    // fs.readFile(path.join(__dirname, 'dataprac.json'))
+    //
+    // const jsonData = JSON.parse(body);
+    // jsonData.push(body);
+    //
+    // fs.writeFile(path.join(__dirname, 'dataprac.json'), JSON.stringify(jsonData, null, 2))
+    //
+
 
 })
 

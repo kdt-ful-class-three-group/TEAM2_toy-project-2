c9b122bc11790267c88d7562834b8cc2045e6372
JadeNmeister <bigdragon00@naver.com>
2025-03-10 01:08:50 +0900

    fs는 nodejs에서만 실행이 돼서 브라우저에서는 fetch를 써서 데이터를 받아와야 함 pagination에서 limit 걸어놓는 것 까ㅏ지 완료, 이제 버튼이랑이벤트 추가 해야함

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3002;
+const PORT = 3000;
 const path = require('path');
 const fs = require('fs');
 
@@ -59,5 +59,5 @@ app.post("/write", function(req,res) {
 })
 
 app.listen(PORT, function() {
-    console.log("http://localhost:3002/")
+    console.log("http://localhost:3000/")
 })
--- public/Pagination.js
+++ public/Pagination.js
@@ -1,10 +1,40 @@
-const fs = require('fs')
 
-let data = fs.readFileSync("data.json").toString()
-
-let dataArray = JSON.parse(data)
+let dataArray = []
 console.log(dataArray)
 let currentPage = 1
+let limit = 8 // 한번에 보여줄 페이지 개수
+let totalPage = Math.ceil(dataArray.length / limit); // 전체페이지 개수를 정함
+
+fetch("/data")// fs는 서버에서만 사용가능하기 때문에 브라우저에서는 fetch를 써서 데이터를 받아와야 함!
+    .then(response => response.json())
+    .then(data => {
+        dataArray = data;
+        totalPage = Math.ceil(dataArray.length / limit);
+        displayData();
+    })
+    .catch(error => console.error("데이터 불러오기 실패:", error));
+
+
+function displayData (){
+    let start = (currentPage - 1 ) * limit
+    let end = start + limit
+    let showingData = dataArray.slice(start, end) // 현재 페이지에서 보여줄 리스트 데이터
+
+    document.getElementById('listDiv').innerHTML = "";
+
+    showingData.forEach(item =>{
+        let div = document.createElement('div')
+        div.textContent = item.title
+        div.setAttribute("style", "cursor:pointer");
+        div.addEventListener("click", function() {
+            readModal(item.title, item.content);
+
+    })
+        document.getElementById("listDiv").appendChild(div);
+
+    })
+}
+
 
 
 // [ ] 총 페이지 개수
@@ -16,5 +46,7 @@ let show = Math.ceil(currentPage / 5)
 
 
 // [ ] 첫번째 페이지 번호
+
+
 
 // [ ] 마지막 페이지 번호

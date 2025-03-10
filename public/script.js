747b0def0ce32c876bde55f572180b72fb194a5c
김윤지 <yunw0117@gmail.com>
2025-03-10 11:23:18 +0900

    상세보기 중 글 쓰기 버튼 금지

--- public/script.js
+++ public/script.js
@@ -29,8 +29,14 @@ texterea.addEventListener("input", function(){
 // 글쓰기 버튼 클릭 시 input 생성
 const writeBtn = document.getElementById("writeBtn")
 writeBtn.addEventListener('click', function() {
-  const form = document.getElementsByTagName('form')[0]
-  form.classList.toggle("display-none")
+  const modal = document.getElementById('modal')
+  if (modal === 'display:none'){
+    const form = document.getElementsByTagName('form')[0]
+    form.classList.toggle("display-none")
+
+  } else {
+    alert('상세보기 중 글 작성이 안됩니다.')
+  }
 
 })
 

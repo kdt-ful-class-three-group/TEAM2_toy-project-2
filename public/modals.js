f45cf5a49510850dae3cf6471d1c368b0f52471c
JadeNmeister <bigdragon00@naver.com>
2025-03-10 21:03:46 +0900

    모달 style 처리 구문

--- public/modals.js
+++ public/modals.js
@@ -1,28 +1,33 @@
 function modals(){
 // 상세보기 모달창, titile, content를 가져오기 위함
     function readModal(title, content) {
-        const modal = document.getElementById("readModal");
-        const modalContent = modal.querySelector("div");
+
+        // 잘못 가져옴 수정  -> modal은 class이므로 querySelector로 가져옴
+        // modal.style.display = "block";을 적용할 때 modal이 아닌 readModal만 처리하고 있음,  modal도 같이 처리해야함
+        const modal = document.querySelector(".modal"); // 모달 전체 배경
+        const readModal = document.getElementById("readModal"); // 모달 창
+
+        const modalContent = readModal.querySelector("div");
 
         modalContent.innerHTML = `
-    <h2>${title}</h2>
-    <p>${content}</p>
-  `;
+        <h2>${title}</h2>
+        <p>${content}</p>
+    `;
 
         modal.style.display = "block";
+        readModal.style.display = "block";
     }
 
-    // 닫기 버튼 클릭 시 modal 닫기
     function closeModal() {
+        const modal = document.querySelector(".modal");
         const readModal = document.getElementById("readModal");
 
-        if (readModal) {
-            readModal.style.display = "none";
-        }
-
+        modal.style.display = "none";
+        readModal.style.display = "none";
     }
+
     window.readModal = readModal;
     window.closeModal = closeModal;
 }
-
+modals();
 export {modals}

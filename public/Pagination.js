0b12bf3cf8d7fb679e6cfedea00664be606647f7
최정민 <bigdragon00@naver.com>
2025-03-10 15:58:06 +0900

    diplayData() 함수 나누기,,,,,,,,,,,성공,,,,,,,,,,

--- public/Pagination.js
+++ public/Pagination.js
@@ -1,9 +1,9 @@
+export let dataArray = [] // 데이터를 받을 배열 선언
 
-let dataArray = [] // 데이터를 받을 배열 선언
-console.log(dataArray)
-let currentPage = 1
-let limit = 5 // 한번에 보여줄 페이지 개수
-let totalPage = Math.ceil(dataArray.length / limit); // 전체페이지 개수를 정함
+export let currentPage = 1
+export let limit = 5 // 한번에 보여줄 페이지 개수
+export let totalPage = Math.ceil(dataArray.length / limit); // 전체페이지 개수를 정함
+import {displayData} from "./displayData.js";
 
 fetch("/data")// fs는 서버에서만 사용가능하기 때문에 브라우저에서는 fetch를 써서 데이터를 받아와야 함!
     .then(response => response.json())
@@ -16,35 +16,11 @@ fetch("/data")// fs는 서버에서만 사용가능하기 때문에 브라우저
     .catch(error => console.error("데이터 불러오기 실패:", error));
 
 
-function displayData (){
-    let start = (currentPage - 1 ) * limit //시작페이지
-    let end = start + limit // 끝페이지
-    let showingData = dataArray.slice(start, end) // 현재 페이지에서 보여줄 리스트 데이터
-
-    document.getElementById('listDiv').innerHTML = ""; // 리스트 초기화 쌓임 방지
-
-// 이 기능이 script.js에 비슷하게 있는데 이 파일에서는 pagination 기능 구현에 필요하기 때문에 추가함
-    showingData.forEach(item =>{
-        let div = document.createElement('div')
-        div.textContent = item.title
-        div.setAttribute("style", "cursor:pointer");
-        div.addEventListener("click", function() {
-            const form = document.getElementsByTagName('form')[0]
-          if(form.classList.contains('display-none')){
-              readModal(item.title, item.content);
-            } else {
-                alert('글 작성 중 상세보기가 안됩니다.')
-            }
-            
-    })
-        document.getElementById("listDiv").appendChild(div);
-
-    })
-}
+ displayData();
 
 
 
-// [ ] 버튼 함수 만들기
+
 
 
 // 이전페이지 함수

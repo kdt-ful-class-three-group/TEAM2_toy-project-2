ca9b7fe7a2e832132cd23c0f2ae0dde02d4dce8e
김민지 <qniomo00@gmail.com>
2025-03-11 09:57:49 +0900

    modals()에 jsDoc 추가

--- public/modals.js
+++ public/modals.js
@@ -1,5 +1,14 @@
+/**
+ * @function modals
+ * modal과 관련된 함수, readModal : modal보여줌, closeModla: modal창 닫기
+ */
 function modals(){
 // 상세보기 모달창, titile, content를 가져오기 위함
+    /**
+     * modal창의 내용 구성하고 목록 클릭했을 때 보여주는 함수
+     * @param {string} title <form>에서 제목에 해당하는 문자열
+     * @param {string} content <form>에서 내용에 해당하는 문자열
+     */
     function readModal(title, content) {
 
         // 잘못 가져옴 수정  -> modal은 class이므로 querySelector로 가져옴
@@ -17,7 +26,10 @@ function modals(){
         modal.style.display = "block";
         readModal.style.display = "block";
     }
-
+    /**
+     * @function closeModal
+     * modal창을 안보기에 하는 함수
+     */
     function closeModal() {
         const modal = document.querySelector(".modal");
         const readModal = document.getElementById("readModal");
